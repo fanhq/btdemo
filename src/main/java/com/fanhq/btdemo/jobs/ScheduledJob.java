c5780f6be7aba4ab8c7f9197a0150b27fdbc58a9
Hachel <fanhq3@asiainfo.com>
2018-01-25 22:57:30 +0800

    初始化

--- src/main/java/com/fanhq/btdemo/jobs/ScheduledJob.java
+++ src/main/java/com/fanhq/btdemo/jobs/ScheduledJob.java
@@ -6,17 +6,12 @@ import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 
-import java.text.SimpleDateFormat;
-
 /**
  * Created by Hachel on 2018/1/25
  */
 @DisallowConcurrentExecution
 public class ScheduledJob implements Job {
 
-    private SimpleDateFormat dateFormat() {
-        return new SimpleDateFormat("HH:mm:ss");
-    }
 
     @Override
     public void execute(JobExecutionContext context) {
