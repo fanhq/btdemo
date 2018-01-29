54ee4784ad6d9525b3f50d5d84daa5c06ea7a64c
Hachel <fanhq3@asiainfo.com>
2018-01-29 16:16:13 +0800

    初始化

--- src/main/java/com/fanhq/btdemo/jobs/ScheduledJob.java
+++ src/main/java/com/fanhq/btdemo/jobs/ScheduledJob.java
@@ -1,15 +1,13 @@
 package com.fanhq.btdemo.jobs;
 
 import com.fanhq.btdemo.service.MyService;
-import org.quartz.DisallowConcurrentExecution;
-import org.quartz.Job;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
+import org.quartz.*;
 
 /**
  * Created by Hachel on 2018/1/25
  */
 @DisallowConcurrentExecution
+@PersistJobDataAfterExecution
 public class ScheduledJob implements Job {
     
 
