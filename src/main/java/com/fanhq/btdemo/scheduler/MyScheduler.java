11eaddf5ddf290632b1a6c4de3aca43d0cfce572
Hachel <fanhq3@asiainfo.com>
2018-01-29 16:38:09 +0800

    初始化

--- src/main/java/com/fanhq/btdemo/scheduler/MyScheduler.java
+++ src/main/java/com/fanhq/btdemo/scheduler/MyScheduler.java
@@ -33,18 +33,20 @@ public class MyScheduler {
         JobDataMap dataMap1 = new JobDataMap();
         dataMap1.put("myService", myService.get(0));
         JobDetail jobDetail = JobBuilder.newJob(ScheduledJob.class).setJobData(dataMap1).build();
-        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0/5 * * * * ?");
         //触发器
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0/5 * * * * ?");
         CronTrigger cronTrigger = TriggerBuilder.newTrigger().withSchedule(scheduleBuilder).build();
+        //调度器
         scheduler.scheduleJob(jobDetail, cronTrigger);
 
         //任务
         JobDataMap dataMap2 = new JobDataMap();
         dataMap2.put("myService", myService.get(1));
         JobDetail jobDetail2 = JobBuilder.newJob(ScheduledJob.class).setJobData(dataMap2).build();
-        CronScheduleBuilder scheduleBuilder2 = CronScheduleBuilder.cronSchedule("0/5 * * * * ?");
         //触发器
+        CronScheduleBuilder scheduleBuilder2 = CronScheduleBuilder.cronSchedule("0/5 * * * * ?");
         CronTrigger cronTrigger2 = TriggerBuilder.newTrigger().withSchedule(scheduleBuilder2).build();
+        //调度器
         scheduler.scheduleJob(jobDetail2, cronTrigger2);
     }
 }
