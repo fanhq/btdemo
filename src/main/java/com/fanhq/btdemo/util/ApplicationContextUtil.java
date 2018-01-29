8270f3988e976a06025ebc83041537b471c323dd
Hachel <fanhq3@asiainfo.com>
2018-01-29 10:11:13 +0800

    初始化

---
+++ src/main/java/com/fanhq/btdemo/util/ApplicationContextUtil.java
@@ -0,0 +1,45 @@
+package com.fanhq.btdemo.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextUtil implements ApplicationContextAware {
+    // Spring应用上下文环境
+    private static ApplicationContext applicationContext;
+
+    /**
+     * 实现ApplicationContextAware接口的回调方法，设置上下文环境
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        ApplicationContextUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 获取对象 这里重写了bean方法，起主要作用
+     *
+     * @return Object 一个以所给名字注册的bean的实例
+     * @throws BeansException
+     */
+    public static Object getBean(String beanId) throws BeansException {
+        return applicationContext.getBean(beanId);
+    }
+    /**
+     * 获取对象 这里重写了bean方法，起主要作用
+     *
+     * @return Object 一个以所给名字注册的bean的实例
+     * @throws BeansException
+     */
+    public static <T> T getBean(Class<T> requiredType) throws BeansException {
+        return applicationContext.getBean(requiredType);
+    }
+
+}
